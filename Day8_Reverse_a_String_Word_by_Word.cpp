5a7b284e2ac6a32dfda21bd6fb4d4452577f8f2e
Aman Khan <amanjkhan21@gmail.com>
2025-08-22 22:18:15 +0530

    Add Day 8 solution: Reverse a String Word by Word

---
+++ Day8_Reverse_a_String_Word_by_Word.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
+
+        reverse(s.begin(), s.end());
+
+        for (int i = 0; i < n; i++) {
+            string word = "";
+            while (i < n && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end()); 
+            if (word.length() > 0) {
+                ans += " " + word;
+            }
+        }
+
+        if (ans.empty()) return "";
+
+        return ans.substr(1);
+    }
+};
+
+int main() {
+    Solution sol;
+    string input = "    ";
+    string result = sol.reverseWords(input);
+    
+    cout << "\"" << result << "\"" << endl;
+    
+}
