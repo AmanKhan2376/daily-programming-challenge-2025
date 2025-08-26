2d3e457ea722597fe0877da2eb52d91832c903c5
Aman Khan <amanjkhan21@gmail.com>
2025-08-26 18:10:16 +0530

    Add Day 12 solution: Valid Parentheses with Multiple Types

---
+++ Day12_Valid_Parentheses.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                if ((c == ')' && st.top() != '(') ||
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main() {
+    string s = "[{()}]";
+
+    if (Solution().isValid(s)) {
+        cout << "Valid parentheses" << endl;
+    } else {
+        cout << "Invalid parentheses" << endl;
+    }
+
+    return 0;
+}
