d5e7657e1ede408ebee1afead4eff6bb6c7d7775
Aman Khan <amanjkhan21@gmail.com>
2025-09-08 19:32:21 +0530

    Add Day 25 solution: Check if a Binary Tree is a Binary Search Tree

---
+++ Day25_Check_if_Binary_Tree_is_Binary_Search_Tree.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+bool isValidBSTHelper(TreeNode* root, long long minVal, long long maxVal) {
+    if (!root) return true;
+    if (root->val <= minVal || root->val >= maxVal) return false;
+
+    return isValidBSTHelper(root->left, minVal, root->val) &&
+           isValidBSTHelper(root->right, root->val, maxVal);
+}
+
+bool isValidBST(TreeNode* root) {
+    return isValidBSTHelper(root, LLONG_MIN, LLONG_MAX);
+}
+
+TreeNode* buildTree(const vector<int>& nodes) {
+    if (nodes.empty() || nodes[0] == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+    while (i < nodes.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (nodes[i] != -1) {
+            curr->left = new TreeNode(nodes[i]);
+            q.push(curr->left);
+        }
+        i++;
+
+        if (i < nodes.size() && nodes[i] != -1) {
+            curr->right = new TreeNode(nodes[i]);
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    vector<int> nodes1 = {5, 1, 4, NULL, NULL, 3, 6};
+    TreeNode* root2 = buildTree(nodes1);
+    cout << (isValidBST(root2) ? "true" : "false") << endl;  
+
+    return 0;
+}
